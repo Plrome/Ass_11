326b8ae184c4bdbd5207cd103a4f11d1c622d91f
DoHongSon <90854969+Plrome@users.noreply.github.com>
2022-02-27 10:59:04 +0700

    Add files via upload

---
+++ Models/ProductModels.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Ass_11.Models;
+
+public class ProductViewModel
+{
+    public int? Id { get; set; }
+    public string? Name { get; set; }
+    public string? Manufacture { get; set; }
+}
+public class ProductCreateModel
+{
+    [Required, MaxLength(50)]
+    public string? Name { get; set; }
+    public string? Manufacture { get; set; }
+}
+
+
+
